74341dfcd2c943e2cf017d1a5fbd8834e98dda4a
Aditya Bhasin <conlini@gmail.com>
2015-11-27 08:53:37 +0530

    WIP: added some tie in to fetch properties - js

--- tech/static/tech/flick/editPropertiesController.js
+++ tech/static/tech/flick/editPropertiesController.js
@@ -1,7 +1,16 @@
 techApp.controller("EditPropertiesController" , ["$scope", "$http", "$mdDialog", function($scope, $http, $mdDialog){
+    $scope.got_properties = false
     $http.get("/tech/rest/categories").then(function(result){
         $scope.categories = result.data["children"]
+        $scope.selected_category = result.data["name"]
         console.log($scope.categories)
 
     }, function(error){})
+    $scope.get_properties_for_categories = function(cat) {
+        $scope.selected_category = cat.name;
+        $http.get("/tech/rest/cat.id/properties").then(function(result){
+            $scope.properties = result.data
+            $scope.got_properties = true
+        }, function(error){});
+    }
 }]);
