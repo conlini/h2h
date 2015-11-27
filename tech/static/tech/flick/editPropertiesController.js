bed8420a595b6848c19fd99247fd86ef068a1d16
Aditya Bhasin <conlini@gmail.com>
2015-11-27 19:02:08 +0530

    WIP: edit mode

--- tech/static/tech/flick/editPropertiesController.js
+++ tech/static/tech/flick/editPropertiesController.js
@@ -1,5 +1,6 @@
 techApp.controller("EditPropertiesController" , ["$scope", "$http", "$mdDialog", function($scope, $http, $mdDialog){
-    $scope.got_properties = false
+    $scope.got_properties = false;
+    $scope.edit_mode = true;
     $http.get("/tech/rest/categories").then(function(result){
         $scope.categories = result.data["children"]
         $scope.selected_category = result.data["name"]
@@ -9,8 +10,12 @@ techApp.controller("EditPropertiesController" , ["$scope", "$http", "$mdDialog",
     $scope.get_properties_for_categories = function(cat) {
         $scope.selected_category = cat.name;
         $http.get("/tech/rest/cat.id/properties").then(function(result){
-            $scope.properties = result.data
-            $scope.got_properties = true
+            $scope.properties = result.data;
+            $scope.got_properties = true;
         }, function(error){});
     }
+    $scope.edit_properties = function() {
+        $scope.edit_mode = true;
+    }
+    
 }]);
